8814803e10c954204321f56beba4ce8b827dbf01
Murli Manohar K <murli6619@gmail.com>
2020-10-11 20:53:31 +0530

    Updates

---
+++ precache-manifest.1fdea8bb451393f26d0f7805caf13ac0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc2c593c1307b2a71a43173a161ce1f",
+    "url": "/roboticbuddie/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboticbuddie/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b3e7d3611a2e39e7550e",
+    "url": "/roboticbuddie/static/css/main.1bf49f81.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/roboticbuddie/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboticbuddie/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e7d3611a2e39e7550e",
+    "url": "/roboticbuddie/static/js/main.7fa430fb.chunk.js"
+  },
+  {
+    "revision": "d7e31c135b9abf0db56d",
+    "url": "/roboticbuddie/static/js/runtime-main.3965e2e8.js"
+  },
+  {
+    "revision": "6666aa591dd35cf642cbf384b49fbf32",
+    "url": "/roboticbuddie/static/media/Needlework Good.6666aa59.woff"
+  }
+]);
